0870f08b8d3f63dbf89670edba409d6ed892087f
Praharsh Pranjal <ppjha.13@gmail.com>
2025-07-27 19:13:18 +0100

    Corrections on folder structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
 import Image from 'next/image'
-import { Link } from 'lucide-react'
+import Link  from 'next/link'
 import { isAuthenticated } from '@/lib/actions/auth'
 import { redirect } from 'next/navigation'
 
@@ -14,7 +14,7 @@ const rootlayout = async ({children}:{children:ReactNode}) => {
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} suppressHydrationWarning/>
           <h2 className="text-primary-100">PrepWise</h2>
         </Link>
       </nav>
